9e161f8b871e2d0fe1f07d32be3f83925b6e318b
Vlad Galaburda <vlad@VladGalaburda.vladgalaburda.com>
2021-10-18 00:59:09 -0400

    modify

--- skileo/skileo/celery.py
+++ skileo/skileo/celery.py
@@ -13,7 +13,6 @@ app.conf.beat_schedule = {
     'add-every-30-seconds': {
         'task': 'everyday.tasks.get_apod_picture',
         'schedule': crontab(hour=0, minute=0), # Every day
-        'args': ("Hello World!",)
     },
 }
 app.conf.timezone = 'UTC'
